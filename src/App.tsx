3f239740eec0b764ab9b604187ce708b27c1e29c
marifroe <marilena.froehlich@gmx.de>
2024-02-14 15:12:42 +0100

    added some content to projects

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,6 @@ import { Contact } from './components/Contact/Contact'
 function App() {
   const sections = [
     {
-      text: 'About',
-      link: '#about',
-    },
-    {
       text: 'Experience',
       link: '#experience',
     },
@@ -38,8 +34,8 @@ function App() {
       <Navbar title='Marilena&#39;s Portfolio' elements={sections} iconMenuSrc='nav/menuIcon.png' iconCloseSrc='nav/closeIcon.png'/>
       <Hero />
       {/*<About title={sections[0].text} elements={aboutElements} />*/}
-      <Experience title={sections[1].text} />
-      <Projects title={sections[2].text} />
+      <Experience title={sections[0].text} />
+      <Projects title={sections[1].text} />
   <Contact title='Contact'/>
     </div>
   )
--- src/pages/ProjectDetails/ProjectDetails.tsx
+++ src/pages/ProjectDetails/ProjectDetails.tsx
@@ -17,7 +17,7 @@ export const ProjectDetails = () => {
       {project
         ?
         <div className={styles.container}>
-          <a href={`/`} className={styles.link}> Back </a>
+          <a href={`/#projects`} className={styles.link}> Back </a>
           <h1 className={styles.title}>{project.title}</h1>
           <div>{project.description}</div>
           {
@@ -26,7 +26,16 @@ export const ProjectDetails = () => {
                 <div key={section.title} className={styles.sectionContainer}>
                   <h2 className={styles.sectionTitle}>{section.title}</h2>
                   <div className={styles.sectionContent}>
-                    <img src={getImageUrl({ path: section.mediaSrc })} alt={`Preview Image for ${section.title}`} className={styles.sectionImage} />
+                    {
+                      (section.mediaType == "img")
+                      ?<img src={getImageUrl({ path: section.mediaSrc })} alt={`Preview Image for ${section.title}`} className={styles.sectionImage} />
+                      :<iframe 
+                        width="560" 
+                        height="315" 
+                        src={section.mediaSrc} 
+                        data-allow="encrypted-media" 
+                        allowFullScreen></iframe>
+                    }
                     <div className={styles.sectionText}>
                         {section.text}
                       </div>
