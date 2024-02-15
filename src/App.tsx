0102142b8018a3eb5e2536b3749292d22f556522
marifroe <marilena.froehlich@gmx.de>
2024-02-15 08:15:35 +0100

    added about section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Navbar } from './components/Navbar/Navbar'
 import { Hero } from './components/Hero/Hero'
 import { Projects } from './components/Projects/Projects'
 import { Contact } from './components/Contact/Contact'
+import { About } from './components/About/About'
 
 function App() {
   const sections = [
@@ -11,6 +12,9 @@ function App() {
       link: '#experience',
     },*/
     {
+      text: 'About',
+      link: '#about',
+    },{
       text: 'Projects',
       link: '#projects',
     },
@@ -32,10 +36,10 @@ function App() {
     <div className={styles.App}>
       <Navbar title='Marilena&#39;s Portfolio' elements={sections} iconMenuSrc='nav/menuIcon.png' iconCloseSrc='nav/closeIcon.png'/>
       <Hero />
-      {/*<About title={sections[0].text} elements={aboutElements} />
-      <Experience title={sections[0].text} />*/}
-      <Projects title={sections[0].text} />
-      <Contact title={sections[1].text}/>
+      <About title={sections[0].text} elements={[]} />
+      {/*<Experience title={sections[0].text} />*/}
+      <Projects title={sections[1].text} />
+      <Contact title={sections[2].text}/>
     </div>
   )
 }
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -13,22 +13,30 @@ type AboutProps = {
 export const About = (props: AboutProps) => {
   return (
     <section className={styles.container} id='about'>
-      <h2 className={styles.title}>{props.title}</h2>
-      <div className={styles.content}>
-        <img className={styles.aboutImg} src={aboutImg} alt='wooden chair' />
-        <ul className={styles.aboutElements}>
-          {props.elements?.map(elem => {
-            return (
-              <li className={styles.aboutElement}>
-                <div className={styles.aboutElementText}>
-                  <h3>{elem.title}</h3>
-                  <p>{elem.text}</p>
-                  </div>
-              </li>
-            )
-          })}
-        </ul>
+      <div className={styles.box}>
+        <h2 className={styles.title}>{props.title}</h2>
+        <div className={styles.content}>
+          I'm a Software Developer who is always up for a good challenge. What I do equally like is designing an
+          intuitive and well-thought-out User Experience and Interface. I love to think of different and novel
+          approaches to achieve a goal and find the best one suitable. Within a team is where I work best - to
+          share and discuss ideas and to get different perspectives. Being an empathic teamplayer, I at the same time
+          work very independently. I'm highly motivated, realiable, structured and a quick learner!
+          {/*<img className={styles.aboutImg} src={aboutImg} alt='wooden chair' />
+          <ul className={styles.aboutElements}>
+            {props.elements?.map(elem => {
+              return (
+                <li className={styles.aboutElement}>
+                  <div className={styles.aboutElementText}>
+                    <h3>{elem.title}</h3>
+                    <p>{elem.text}</p>
+                    </div>
+                </li>
+              )
+            })}
+          </ul>*/}
+        </div>
       </div>
+      
     </section>
   )
 }
