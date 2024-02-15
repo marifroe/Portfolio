2fdeb1f4749466bf1394a51d3316b21e18e0222d
marifroe <marilena.froehlich@gmx.de>
2024-02-15 01:45:34 +0100

    remove experience declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
 import { Hero } from './components/Hero/Hero'
-import { Experience } from './components/Experience/Experience'
 import { Projects } from './components/Projects/Projects'
 import { Contact } from './components/Contact/Contact'
 
