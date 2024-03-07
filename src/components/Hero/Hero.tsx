1d50fe7359b5717f19de77850b96aac597dcef3c
marifroe <marilena.froehlich@gmx.de>
2024-03-07 10:35:04 +0100

    add note to hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Marilena.</h1>
-        <p className={styles.description}>A Software Developer who loves to solve Problems in creative Ways.</p>
+        <p className={styles.description}>I'm a Software Developer who loves to solve problems in creative ways.<br/><span className={styles.unhighlight}>( My portfolio is still work in progress. As perfectionist it's something I find hard to say: But please don't expect perfectionism at this point :&#41; )</span></p>
         <div className={styles.buttons}>
           <a className={styles.heroBtn} href="mailto:marilena.froehlich@gmx.de">Contact Me</a>
           <a className={styles.heroBtn} href="#projects">Projects</a>
