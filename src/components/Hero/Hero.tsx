837ff3bc237bca2be6b4294c318a35444d2326ea
marifroe <marilena.froehlich@gmx.de>
2024-03-09 00:07:42 +0100

    add hero description and change button design; adjust mobile about layout

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         <h1 className={styles.title}>Hi, I'm Marilena.</h1>
         <p className={styles.description}>I'm a Software Developer who loves to solve problems in creative ways.<br/><span className={styles.unhighlight}>my portfolio is work in progress and still has a few quirks here and there, sorry for the inconvenience </span></p>
         <div className={styles.buttons}>
-          <a className={styles.heroBtn} href="mailto:marilena.froehlich@gmx.de">Contact Me</a>
+          <a className={`${styles.heroBtn} ${styles.unfilled}`} href="mailto:marilena.froehlich@gmx.de">Contact Me</a>
           <a className={styles.heroBtn} href="#projects">Projects</a>
         </div>
       </div>
