5274745ea85e1ac1b223dbb2114c326c25df4bbd
marifroe <marilena.froehlich@gmx.de>
2024-03-08 23:35:25 +0100

    adjust navbar and hero for mobile

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Marilena.</h1>
-        <p className={styles.description}>I'm a Software Developer who loves to solve problems in creative ways.<br/><span className={styles.unhighlight}>( My portfolio is still work in progress. As perfectionist it's something I find hard to say: But please don't expect perfectionism at this point :&#41; )</span></p>
+        <p className={styles.description}>I'm a Software Developer who loves to solve problems in creative ways.<br/><span className={styles.unhighlight}>my portfolio is work in progress and still has a few quirks here and there, sorry for the inconvenience </span></p>
         <div className={styles.buttons}>
           <a className={styles.heroBtn} href="mailto:marilena.froehlich@gmx.de">Contact Me</a>
           <a className={styles.heroBtn} href="#projects">Projects</a>
